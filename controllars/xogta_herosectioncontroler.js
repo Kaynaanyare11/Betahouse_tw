2ce31873ab17ca87c71a5a0f950aa0e2e6d8bfdf
ruwaruwa <almozali287@gmail.com>
2023-08-04 14:28:48 +0300

    xogtashirkada new update

--- controllars/xogta_herosectioncontroler.js
+++ controllars/xogta_herosectioncontroler.js
@@ -4,7 +4,7 @@ const joi= require('joi')
 //get al
 const GET=async(req,res)=>{
 try {
-    const orgnizationDAta= await shirkadamodel.find()
+    const orgnizationDAta= await shirkadamodel.find().sort({_id: -1}).limit(1)
     res.status(200).send(orgnizationDAta)
 } catch (error) {
     res.status(400).send(error.message)
@@ -43,28 +43,49 @@ const shirkadvalidation=(shirkadvalida)=>{
 }
 //post
 const POST=async(req,res)=>{
+try {
+        
 const{err}=shirkadvalidation(req.body)
-if(err){
-    res.status(400).send(err.message)
+if(err) return  res.status(400).send(err.message)
+const getshirkada= await shirkadamodel.find().sort({_id: -1}).limit(1)
+if(getshirkada){
+    const shirkadaUpdate= await shirkadamodel.findByIdAndUpdate(getshirkada[0]._id,{
+        name:req.body.name,
+        location:req.body.location,
+        logo:req.body.logo,
+        address:req.address,
+        email:req.body.email,
+        phone:req.body.phone,
+        facebook:req.body.facebook,
+        tiktok:req.body.tiktok,
+        twitter:req.body.twitter,
+        Instigram:req.body.Instigram,
+        her_title:req.body.her_title,
+        hero_img:req.body.hero_img,
+        hero_description:req.body.hero_description,
+        footer_Text:req.body.footer_Text
+    },{new:true})
+    res.status(200).send({shirkadaUpdate})
 }
-    try {
-      const xogtashikada= new shirkadamodel(req.body) 
+    else{
+        const xogtashikada= new shirkadamodel(req.body)
        await xogtashikada.save();
-       res.status(200).send({message:"successfuly posted",homesection:xogtashikada})
-    } catch (error) {
-        res.status(400).send(error.message)
+       res.status(200).send({status:true,message:"successfuly posted",homesection:xogtashikada})
     }
+} catch (error) {
+    res.status(400).send(error.message)
+}
 }
 //pu
-const update=async(req,res)=>{
-    try {
-       const {id}=req.params
-       const upd= await shirkadamodel.findByIdAndUpdate(id,req.body,{new:true}) ;
-       res.status(200).send("successfuly updated")
-    } catch (error) {
-     res.status(400).send(error.message)   
-    }
-}
+// const update=async(req,res)=>{
+//     try {
+//        const {id}=req.params
+//        const upd= await shirkadamodel.findByIdAndUpdate(id,req.body,{new:true}) ;
+//        res.status(200).send("successfuly updated")
+//     } catch (error) {
+//      res.status(400).send(error.message)   
+//     }
+// }
 //delete
 const deletes=async(req,res)=>{
     try {
@@ -75,4 +96,4 @@ const deletes=async(req,res)=>{
         res.status(400).send(error.message)
     }
 }
-module.exports={GET,GETONE,POST,update,deletes}
+module.exports={GET,GETONE,POST,deletes}
